cf8e40b6bf2b774c14a846724b0447fc688e21d4
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-22 17:17:18 +0000

    variables configure

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId=40626
+let accountEmail="adil.thakur@servicenow.com"
+
+var a=-1;
+let b ;
+const c=a;
+console.log(a,b,c);
+var password="12345"
+
+var password="12345"
+accountCity="hyderabad"
+
+accountEmail="adilthakur7777@gmail.com";
+console.log(accountEmail,accountId,accountCity);
+console.table([accountEmail,accountId,password,accountCity])
