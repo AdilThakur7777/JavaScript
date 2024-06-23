1f17c0b82661ab1e8393febe07ccd547e33cc3e2
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-23 07:03:23 +0000

    strings

---
+++ basics/string.js
@@ -0,0 +1,40 @@
+const name="adil "
+const repoCount=50
+// console.log(name +repoCount + " values "); outdated
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName= new String("Adil-Thakur")
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(10));
+console.log(gameName.indexOf('u'));
+
+
+//main methods 
+// const newString = gameName.substring(0,)
+
+// console.log(newString);
+
+
+const anotherString=gameName.slice(-11,5)
+console.log(anotherString);
+
+
+
+const newString1="     adil     thkaur        "
+console.log(newString1);
+console.log(newString1.trim());
+
+
+const url="https://adil.com/adil-thakurs"
+
+console.log(url.replace("adil","saristy"))
+
+
+console.log(url.includes('thakur'))
+
+
+console.log(gameName.split("-"));
+
