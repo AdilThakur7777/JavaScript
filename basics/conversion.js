a1ac630565b686ce4ce337503da9bd620120ba56
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-23 05:11:41 +0000

    coversion of datatypes

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+let score=true
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+//"33" =>33
+// "33abc"=>NaN
+
+let isLogIn=" "
+let booleanIsLoggedIn=Boolean(isLogIn)
+console.log(booleanIsLoggedIn)
