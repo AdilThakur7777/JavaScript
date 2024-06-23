e019c6a0a0eda8e69304052448a5548096692c75
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-23 14:17:11 +0000

    scope

---
+++ basics2/scopes.js
@@ -0,0 +1,20 @@
+//scope in javascript
+// let var const/
+let a = 1000000n
+var c = 300
+
+if(true)
+    {
+        let a = 10
+        console.log(a);
+        const b = 20
+        var c = 30
+    }
+
+console.log(a);
+// console.log(b);
+console.log(c);
+
+const i=0;
+
+console.log(i);
