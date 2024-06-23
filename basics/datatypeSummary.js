72352221760efbd244647c5e2c1cb1b339be8820
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-23 06:17:42 +0000

    datamore

---
+++ basics/datatypeSummary.js
@@ -0,0 +1,38 @@
+// primitive and non primitive 
+//7
+/* 
+primitive types are call by values 
+Number
+BigInt
+String
+Null
+Undefined
+Symbol
+Boolean
+is javascript static typed or dynamic typed language ?
+*/
+// const id=Symbol('1111123')
+// const anotherId =Symbol('1230')
+
+// console.log(id===anotherId)
+// console.log(anotherId==id);;
+//Non primitive (Reference type)
+// Arrays
+// Objects
+// Functions
+//
+const bigInt=2343225535n
+const heros=["Batman","Superman","Spiderman"]
+let myObj={
+    name:"Adil",
+    age:"23",
+}
+const myFunc=function(){
+    console.log("helllo world");
+}
+
+
+console.log(typeof myFunc)
+console.log(typeof null)
+console.log(typeof myObj)
+console.log(typeof myObj)
