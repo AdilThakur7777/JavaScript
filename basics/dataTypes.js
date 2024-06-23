1c7d4abcb0c6e705692ff65e2f4e53a99db62080
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-23 05:13:04 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,17 @@
+"user Strict" ;//treat all js code as newer version
+// alert("hey"). error in real time
+let name="adil "
+console.log(indexof("adilll",0,1));
+
+
+
+//number => 2^53
+//bigint
+//String
+//boolean
+//null
+//undefined
+//symbol
+//object
+console.log(typeof undefined)
+
