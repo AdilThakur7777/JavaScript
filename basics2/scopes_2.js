4dd2270601cf8c99f8b90ce0b7d8eadb93063d9c
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-23 18:21:53 +0000

    morescopes

---
+++ basics2/scopes_2.js
@@ -0,0 +1,33 @@
+// function one()
+// {
+//     const userName = "adil "
+//     function two()
+//     {
+//         const webSite='youtube'
+//         console.log(userName)
+//     }
+//     // console.log(webSite);//error
+//     // two()
+// }
+// one()
+
+
+///************************important concept */
+
+
+
+
+
+
+console.log(addone(5))
+function addone(num )
+{
+    return num+1
+}
+
+
+const addTwo = function(num) //expression
+{
+    return num+2
+}
+console.log(addTwo(5))
