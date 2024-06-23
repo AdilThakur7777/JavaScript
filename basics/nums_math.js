3647c2f4364a6f66e0fdf67a93ba2194a13d704e
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-23 07:31:55 +0000

    maths and num

---
+++ basics/nums_math.js
@@ -0,0 +1,34 @@
+// const score =1000
+// const balance = new Number(90000)
+
+// console.log(balance);
+// console.log(score);
+// console.log(typeof balance);
+// console.log(typeof score);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const otherNumber=123.4999999
+
+// console.log(otherNumber.toPrecision(3));
+
+// const hundreds =100000000
+// console.log(hundreds.toLocaleString('en-IT'));
+
+
+//++++++++++++++++ Maths +++++++++++++++++++//
+
+console.log(Math.abs(-11));
+console.log(Math.round(4.5));
+console.log(Math.ceil(4.5))
+console.log(Math.floor(4.9));
+
+console.log((Math.random()*10)+1);
+
+
+const min=1
+const max=6
+
+
+console.log(Math.floor(Math.random() *(max-min+1))+ min)
