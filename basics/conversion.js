0d4a911fbd395aeeeb9e5676568a87100659f26c
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-23 05:32:25 +0000

    conversions

--- basics/conversion.js
+++ basics/conversion.js
@@ -3,12 +3,47 @@ let score=true
 // console.log(typeof score)
 
 // let valueInNumber = Number(score);
-// console.log(typeof valueInNumber)
-// console.log(valueInNumber);
+// // console.log(typeof valueInNumber)
+// // console.log(valueInNumber);
 
-//"33" =>33
-// "33abc"=>NaN
+// //"33" =>33
+// // "33abc"=>NaN
 
-let isLogIn=" "
-let booleanIsLoggedIn=Boolean(isLogIn)
-console.log(booleanIsLoggedIn)
+// let isLogIn=" "
+// let booleanIsLoggedIn=Boolean(isLogIn)
+// console.log(booleanIsLoggedIn)
+
+// Operations 
+
+let value =3
+let negValue=-value
+//console.log(negValue)
+
+// console.log(3%9);
+
+// let str1="adil"
+// let str2="thakur"
+// console.log(str1+str2);
+
+// console.log("1"+2)
+// console.log(1+"2"+2*2)
+// console.log("1"+2+2);
+// console.log(1+2+"2")
+// console.log(3+4*5%3);
+// console.log(+false);
+// console.log(+true);
+
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2 + 2
+// let a=10
+// let b=++a
+// console.log(b);
+// console.log(a);
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter);
+
+
+
