617d48f42783ff15d3648422e0016dda608048c0
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-24 18:58:06 +0000

    flow

---
+++ ControlFlow/one.js
@@ -0,0 +1,25 @@
+//if
+// // <,> <=,>=,==,===,!=
+//     if(10==='10')
+//     {
+//         console.log('hq');
+//     }
+
+
+const score =200
+// if(score>100)
+//     {
+//             const power='fly'
+//             console.log(`user power : ${power}`);
+//     }
+//     console.log(`user power : ${power}`);
+// //errors are good
+
+//     if(score>100)
+//         {
+//                 var power='fly'
+//                 console.log(`user power : ${power}`);
+//         }
+//         console.log(`user power : ${power}`);
+//         // no error are bad
+        if(score>100) console.log('hw');
