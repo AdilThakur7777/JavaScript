9c238ff968e4fcf0fd5f52254fb1a505f2f39a4f
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-24 17:54:50 +0000

    arrow

--- basics2/arrowFunc.js
+++ basics2/arrowFunc.js
@@ -4,7 +4,7 @@
 //     WelcomeMessage : function()
 //     {
 //         console.log(`${this.userName} Welcome to the website`);
-//         console.log(this)
+    //    console.log(this)
 //     }
 // }                
 
@@ -13,8 +13,41 @@
 // user.WelcomeMessage()
 
 
-function chai()
-{
-    console.log(this);
-}
-chai()
+// function chai()
+// {
+//     console.log(this);
+// }
+// chai()
+// Arrow Function
+
+
+// const chai  = ()=>
+// {
+//     let username='hitesh'
+//     console.log(this.username);
+// }
+// chai()
+
+// const addTwo = (num1, num2) =>
+//     {
+//         return num1 + num2
+//     }
+
+// console.log(addTwo(1000,20000))
+
+// const addTwo = (num1, num2) => num1 * num2 
+
+
+// const addTwo = (num1, num2) => (num1 * num2 )
+
+// const addTwo = (num1, num2) => { return num1 * num2 }
+
+// const addTwo = (num1, num2) => ({username:"Adil"}) objects ko paranthesis se wrap krte he 
+
+// console.log(addTwo(10,8));
+
+
+const myArr=[12,3,5,6,7,8]
+
+// myArr.forEach(()=>{})
+    
