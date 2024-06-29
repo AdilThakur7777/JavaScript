34be38b7494a2f289c0dc14f3be3d7bc47a83b74
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-29 10:05:49 +0000

    swtich

--- ControlFlow/one.js
+++ ControlFlow/one.js
@@ -1,12 +1,12 @@
 //if
-// // <,> <=,>=,==,===,!=
+// // <,> <=,>=,==,===,!=,!==
 //     if(10==='10')
 //     {
 //         console.log('hq');
 //     }
 
 
-const score =200
+// const score =200
 // if(score>100)
 //     {
 //             const power='fly'
@@ -22,4 +22,33 @@ const score =200
 //         }
 //         console.log(`user power : ${power}`);
 //         // no error are bad
-        if(score>100) console.log('hw');
+        // if(score>100) console.log('hw');
+
+
+
+        // switch(Key)
+        // {
+        //         case value:
+        //                 break;
+        //         case value2 :
+        //                 break;
+        //         default :
+        //                 break;
+        // }
+
+
+const month=5
+switch (month) {
+        case 1:
+               console.log("January "); 
+                break;
+        case 2:
+                console.log("Feburary");
+                break;
+        case 3:
+                console.log("March");
+                break;
+        default:
+                console.log("nothing");
+                break;
+}
