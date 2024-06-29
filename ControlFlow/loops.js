cf056304bed30bf60f52dccc23ee942cd6791247
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-29 10:53:31 +0000

    do and while loops also

--- ControlFlow/loops.js
+++ ControlFlow/loops.js
@@ -1,13 +1,23 @@
 // for loop
 
-const index =[1,2,3,4,5,6]
+// const index =[1,2,3,4,5,6]
 
-for(let i =0 ;i<index.length; i++)
-    {
-        if(index[i]==5) continue
-        console.log(index[i]);
+// for(let i =0 ;i<index.length; i++)
+//     {
+//         if(index[i]==5) continue
+//         console.log(index[i]);
         
-    }
+//     }
 
 // break and continue
+//while 
+let index=0
+while(index<10)
+    {
+        console.log(`value of index is ${index}`);
+        index++
+    }
 
+// do {
+    
+// } while (condition);
