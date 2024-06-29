0541315d4e7c86ccc0a07965389fee012c8a48a0
Adil Thakur <74560934+AdilThakur7777@users.noreply.github.com>
2024-06-29 10:48:04 +0000

    loops

---
+++ ControlFlow/loops.js
@@ -0,0 +1,13 @@
+// for loop
+
+const index =[1,2,3,4,5,6]
+
+for(let i =0 ;i<index.length; i++)
+    {
+        if(index[i]==5) continue
+        console.log(index[i]);
+        
+    }
+
+// break and continue
+
